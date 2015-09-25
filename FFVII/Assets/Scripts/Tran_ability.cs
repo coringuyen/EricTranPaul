068518f5b0aa0d79a015ba70f6b35497adffa8ae
coringuyen <coringuyen96@gmail.com>
2015-09-25 12:14:01 -0500

    color

--- FFVII/Assets/Scripts/Tran_ability.cs
+++ FFVII/Assets/Scripts/Tran_ability.cs
@@ -6,11 +6,11 @@ public class Tran_ability : MonoBehaviour
     string name_of_ability;
     int damages;
 
-	void creatAbility(string ability_name, string name_color)
+	void creatAbility(string ability_name, Color a_color)
     {
         name_of_ability = ability_name;
         damages = Random.Range(20, 50);
-        this.gameObject.GetComponent<Renderer>().material.GetColor(name_color);
+        gameObject.GetComponent<Renderer>().material.color = a_color;
     }
     
 }
