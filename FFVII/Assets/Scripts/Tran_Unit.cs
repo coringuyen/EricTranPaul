db001587fe80320fe1db6e8c87350f40faa14c3f
coringuyen <coringuyen96@gmail.com>
2015-09-25 11:06:01 -0500

    Working on Unit

---
+++ FFVII/Assets/Scripts/Tran_Unit.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Tran_Unit : MonoBehaviour, baseClass
+{
+    public int unit_health;
+    public int attack_damages;
+    public int ability_damages;
+
+    void Start()
+    {
+        unit_health = health;
+    }
+
+    public int health
+    {
+        get
+        {
+            return unit_health;
+        }
+
+        set
+        {
+            unit_health = value;
+        }
+    }
+
+    public void ability()
+    {
+        takeDamage(ability_damages);
+    }
+
+    public void attack()
+    {
+        takeDamage(attack_damages);
+    }
+
+    public void takeDamage(int damage)
+    {
+        health -= damage;
+    }
+}
---
+++ FFVII/Assets/Scripts/Tran_baseClass.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public interface baseClass
+{
+    int health
+    {
+        get; set;
+    }
+
+    void attack();
+
+    void takeDamage(int damage);
+
+    void ability();
+}
+
