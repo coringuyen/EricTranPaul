fa05e55aa91f9e05a5eecaf26362c7683f9b17f6
Chui <lordbabypanda@gmail.com>
2015-09-25 11:06:01 -0500

    GUI is done
    BOOM!

---
+++ FFVII/Assets/Scripts/GUI_PC.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUI_PC : MonoBehaviour
+{
+    public Slider player1Health;
+    public Slider player2Health;
+    public Slider player3Health;
+
+    public Button attack;
+    public Button specialAttack;
+    public Button endTurn;
+
+    void displayHealth(int damage, int health)
+    {
+        player1Health.value = health;
+    }
+
+}
