6366c086589d909a5da2fe05be49b1ea8bf32539
Chui <lordbabypanda@gmail.com>
2015-09-25 12:10:40 -0500

    Done with GUI!

--- FFVII/Assets/Scripts/GUI_PC.cs
+++ FFVII/Assets/Scripts/GUI_PC.cs
@@ -12,9 +12,45 @@ public class GUI_PC : MonoBehaviour
     public Button specialAttack;
     public Button endTurn;
 
-    void displayHealth(int damage, int health)
+    float health = 100;
+
+    void Update()
     {
-        player1Health.value = health;
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            health_Bar();
+        }
     }
 
+    public void health_Bar()
+    {
+        health = player1Health.value;
+        health -= 1;
+        player1Health.value = health;
+        print("IS IT WORKING?!?");
+    }
+
+    public void attackSomeBody()
+    {
+        if (attack)
+        {
+            print("Attack somebody!");
+        }
+    }
+
+    public void special_Attack()
+    {
+        if (specialAttack)
+        {
+            print("Charging up my SUPER POWER!");
+        }
+    }
+
+    public void turn()
+    {
+        if (endTurn)
+        {
+            print("Ending my turn");
+        }
+    }
 }
