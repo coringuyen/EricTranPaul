6b4f1f31a3aa7c42e63b94cf6b068844c9755cb6
coringuyen <coringuyen96@gmail.com>
2015-09-28 23:39:57 -0500

    comment code

--- FFVII/Assets/Scripts/Tran_Unit.cs
+++ FFVII/Assets/Scripts/Tran_Unit.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class Tran_Unit : MonoBehaviour, baseClass
 {
-    public Color m_color;
-    public int target_heath;
+    public Color m_color; // change color when Unit use special ability
     public int unit_health;
     public int attack_damages;
     public int ability_damages;
 
-    public string name_of_ability;
+    public string name_of_ability; 
 
     void Start()
     {
@@ -32,21 +31,25 @@ public class Tran_Unit : MonoBehaviour, baseClass
 
     public void specialAbility(GameObject target)
     {
+        // take target health away depend on however much special ability damage is
         target.GetComponent<Tran_Unit>().takeDamage(ability_damages);
-        gameObject.GetComponent<Renderer>().material.color = m_color;
-        Debug.Log(name_of_ability);
+        gameObject.GetComponent<Renderer>().material.color = m_color; // change color
+        Debug.Log(name_of_ability); // print out to console the name of the special ability
     }
 
     public void attack(GameObject target)
     {
+        // take target health away depend on however much attack damage is
         target.GetComponent<Tran_Unit>().takeDamage(attack_damages);
     }
 
-    public void takeDamage(int damage)
+    // take away health by subtracting from damage 
+    public void takeDamage(int damage) 
     {
         health -= damage;
     }
 
+    // generate a random number for special ability damage
     public void createAbility()
     {
         ability_damages = Random.Range(20, 50);
